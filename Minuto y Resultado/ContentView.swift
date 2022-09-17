2fd699c4a1f055fd5e70a8eaee31a3f10d6dc24b
victormanueldelriogarcia <vmdelrio@gmail.com>
2022-09-17 07:20:03 +0200

    Initial Commit

---
+++ Minuto y Resultado/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Minuto y Resultado
+//
+//  Created by Victor Manuel Del Rio Garcia on 17/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Minuto y Resultado/Minuto_y_ResultadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Minuto_y_ResultadoApp.swift
+//  Minuto y Resultado
+//
+//  Created by Victor Manuel Del Rio Garcia on 17/9/22.
+//
+
+import SwiftUI
+
+@main
+struct Minuto_y_ResultadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
